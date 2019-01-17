8e4c8367ee0b05944fa85b6e63797b4fa02ca2c5
chris-gong <chris_gong@yahoo.com>
2019-01-17 17:38:54 -0500

    made more updates on the map and added switching between different materials when building

---
+++ Source/FortniteClone/Private/MaterialActor.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MaterialActor.h"
+
+// Sets default values
+AMaterialActor::AMaterialActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMaterialActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMaterialActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/FortniteClone/Public/MaterialActor.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MaterialActor.generated.h"
+
+UCLASS()
+class FORTNITECLONE_API AMaterialActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMaterialActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Material")
+	int MaterialType; // 0 for wood, 1 for brick, 2 for steel
+
+	UPROPERTY(EditDefaultsOnly, Category = "Material")
+	int MaterialCount;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Health")
+	int Health;
+};
