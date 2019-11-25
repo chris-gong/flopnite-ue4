543c0889ba13c3fbc692cc9c3c0ad246df8e2321
pivotman319 <pivotman2003@gmail.com>
2019-11-25 20:17:51 +0100

    TVONSCLU - Major Update #1: Lobby Overhaul
    - LobbyCharacter now has a proper HUD and camera position

--- Source/FortniteClone/Private/LobbyHUD.cpp
+++ Source/FortniteClone/Private/LobbyHUD.cpp
@@ -1,16 +1,28 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "LobbyHUD.h"
+#include "Engine/Canvas.h"
+#include "Engine/Texture2D.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Blueprint/UserWidget.h"
+#include "Engine/Engine.h"
 
 ALobbyHUD::ALobbyHUD()
 {
+	// copied from FortniteCloneHUD class
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairObj(TEXT("/Game/UI/Foundation/Textures/Player/T_Crosshair"));
+	CrosshairTexture = CrosshairObj.Object;
 
+	static ConstructorHelpers::FClassFinder<UUserWidget> CountObj(TEXT("/Game/UI/Widgets/UI_PlayersJoinedCount"));
+	PlayersJoinedCountWidgetClass = CountObj.Class;
+	static ConstructorHelpers::FClassFinder<UUserWidget> WaitingForPlayersObj(TEXT("/Game/UI/Widgets/UI_WaitingForPlayers"));
+	WaitingForPlayersWidgetClass = WaitingForPlayersObj.Class;
 }
 
 void ALobbyHUD::DrawHUD()
 {
 	Super::DrawHUD();
+	DrawCrosshair();
 }
 
 
@@ -25,5 +37,42 @@ void ALobbyHUD::BeginPlay()
 		PlayerController->bEnableClickEvents = false;
 		PlayerController->bEnableMouseOverEvents = false;
 		PlayerController->SetInputMode(FInputModeGameOnly());
+	}
+	DrawGameUI();
+}
+
+void ALobbyHUD::DrawCrosshair() {
+	// find center of the Canvas
+	const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
+
+	// offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
+	const FVector2D CrosshairDrawPosition((Center.X - CrosshairTexture->GetSizeX() / 2),
+		(Center.Y - CrosshairTexture->GetSizeY() / 2));
+
+	// draw the crosshair
+	FCanvasTileItem CrosshairTileItem(CrosshairDrawPosition, CrosshairTexture->Resource, FLinearColor::White);
+	CrosshairTileItem.BlendMode = SE_BLEND_Translucent;
+	Canvas->DrawItem(CrosshairTileItem);
+}
+
+void ALobbyHUD::DrawGameUI(){
+	if (PlayersJoinedCountWidgetClass != nullptr)
+	{
+		CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), PlayersJoinedCountWidgetClass);
+
+		if (CurrentWidget)
+		{
+			CurrentWidget->AddToViewport();
+		}
+	}
+
+	if (WaitingForPlayersWidgetClass != nullptr)
+	{
+		CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), WaitingForPlayersWidgetClass);
+
+		if (CurrentWidget)
+		{
+			CurrentWidget->AddToViewport();
+		}
 	}
 }
