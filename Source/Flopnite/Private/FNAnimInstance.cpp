ba92dccead2e8068ea623d8fdcd4ef7d3d2b5011
chris-gong <chris_gong@yahoo.com>
2020-10-09 13:09:09 -0400

    created new anim instance class

---
+++ Source/Flopnite/Private/FNAnimInstance.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "FNAnimInstance.h"
+
---
+++ Source/Flopnite/Public/FNAnimInstance.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "FNAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLOPNITE_API UFNAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
