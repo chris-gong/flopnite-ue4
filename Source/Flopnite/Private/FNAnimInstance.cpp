ad856b46187d161b5ad18dd5ff7a9f83495ab9c3
BEASTSM96 <49040508+BEASTSM96@users.noreply.github.com>
2020-10-10 14:51:55 +0100

    Update stuff

--- Source/Flopnite/Private/FNAnimInstance.cpp
+++ Source/Flopnite/Private/FNAnimInstance.cpp
@@ -1,5 +1,52 @@
-
-
+//
 
 #include "FNAnimInstance.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "FNCharacter.h"
 
+UFNAnimInstance::UFNAnimInstance()
+{
+    /* */
+    IsInAir = false;
+    Direction = 0.0;
+}
+
+void UFNAnimInstance::NativeUpdateAnimation( float DeltaSeconds )
+{
+    Super::NativeUpdateAnimation( DeltaSeconds );
+
+    if ( TryGetPawnOwner() != nullptr )
+    {
+        Speed = TryGetPawnOwner()->GetVelocity().Size();
+        Direction = CalculateDirection( TryGetPawnOwner()->GetVelocity(),  TryGetPawnOwner()->GetActorRotation() );
+
+        AFNCharacter * OwningChar = Cast< AFNCharacter >( TryGetPawnOwner() );
+        if (OwningChar != nullptr)
+        {
+            IsInAir = OwningChar->GetCharacterMovement()->IsFalling();
+        }
+        
+    }
+    
+}
+
+void UFNAnimInstance::NOTIFY_Jump()
+{
+    AFNCharacter * OwningChar = Cast< AFNCharacter >( TryGetPawnOwner() );
+
+    if (EnableJump)
+    {
+        OwningChar->Jump();        
+    }
+    
+}
+
+void UFNAnimInstance::NOTIFY_JogStart()
+{
+    AFNCharacter * OwningChar = Cast< AFNCharacter >( TryGetPawnOwner() );
+
+    EnableJump = false;
+
+    //SomeThing here
+    
+}
--- Source/Flopnite/Public/FNAnimInstance.h
+++ Source/Flopnite/Public/FNAnimInstance.h
@@ -9,9 +9,34 @@
 /**
  * 
  */
-UCLASS()
+UCLASS(Blueprintable)
 class FLOPNITE_API UFNAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+	
+public:
+	UFNAnimInstance(  );
+	
+public:
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category="Generic" )
+	float Speed;
+
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category="Generic" )
+	bool IsInAir;
+
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category="Generic" )
+	float Direction;
+	
+	virtual void NativeUpdateAnimation( float DeltaSeconds ) override;
+	
+public:
+	UFUNCTION( BlueprintCallable )
+	void NOTIFY_Jump();
+
+	UFUNCTION( BlueprintCallable )
+	void NOTIFY_JogStart();
+
+	UPROPERTY( BlueprintReadWrite, EditAnywhere )
+	bool EnableJump;
 	
 };
