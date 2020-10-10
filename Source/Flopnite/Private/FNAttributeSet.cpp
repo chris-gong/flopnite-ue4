be86f69add3d2fdfc0d856e5f8d187bcad14deb0
BEASTSM96 <49040508+BEASTSM96@users.noreply.github.com>
2020-10-10 15:06:29 +0100

    Fixed

--- Source/Flopnite/Private/FNAttributeSet.cpp
+++ Source/Flopnite/Private/FNAttributeSet.cpp
@@ -24,10 +24,5 @@ void UFNAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutL
 
 void UFNAttributeSet::OnRep_MoveSpeed(const FGameplayAttributeData& OldMoveSpeed)
 {
-<<<<<<< HEAD
 	GAMEPLAYATTRIBUTE_REPNOTIFY( UFNAttributeSet, MoveSpeed, OldMoveSpeed );
 }
-=======
-	GAMEPLAYATTRIBUTE_REPNOTIFY(UFNAttributeSet, MoveSpeed);
-}
->>>>>>> fee491b... fix GAMEPLAYATTRIBUTE_REPNOTIFY
