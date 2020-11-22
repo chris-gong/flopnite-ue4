d1b7dae483ced61b394c53ec1952c8beca6ea6d6
chris-gong <chris_gong@yahoo.com>
2020-11-22 12:05:53 -0500

    modified default class names and game mode in defaultengine.ini, literally copy and pasted als blueprints to the thirdpersoncpp folder in order to get all the event graph code in addition to variable values, and modified gamemode to use the new blueprints in the thirdpersoncpp folder

--- Source/Flopnite/Private/FNGameMode.cpp
+++ Source/Flopnite/Private/FNGameMode.cpp
@@ -1,17 +1,19 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "FNGameMode.h"
-#include "FNCharacter.h"
 #include "FNPlayerState.h"
 #include "UObject/ConstructorHelpers.h"
 
 AFNGameMode::AFNGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/ALSV4_CPP/AdvancedLocomotionV4/Blueprints/CharacterLogic/ALS_CharacterBP"));
-	if (PlayerPawnBPClass.Class != NULL)
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonPlayerController"));
+
+	if (PlayerPawnBPClass.Class != NULL && PlayerControllerBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
+		PlayerControllerClass = PlayerControllerBPClass.Class;
 		PlayerStateClass = AFNPlayerState::StaticClass();
 	}
 }
