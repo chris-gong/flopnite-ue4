9d70d1a5a1736826ff48a39e97e54c83efa37bdd
chris-gong <chris_gong@yahoo.com>
2020-11-28 14:47:41 -0500

    added als cpp plugin as a public dependency module

--- Source/Flopnite/Flopnite.Build.cs
+++ Source/Flopnite/Flopnite.Build.cs
@@ -8,6 +8,6 @@ public class Flopnite : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "ALSV4_CPP" });
 	}
 }
