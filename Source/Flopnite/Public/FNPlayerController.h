567bb53f55f83e11b23d5cbde9f6601599f5800f
chris-gong <chris_gong@yahoo.com>
2020-11-28 19:47:48 -0500

    created new player controller class, subclasses aalsplayercontroller and reparented thirdpersonplayercontroller bp to this subclass

---
+++ Source/Flopnite/Public/FNPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ALSPlayerController.h"
+#include "FNPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLOPNITE_API AFNPlayerController : public AALSPlayerController
+{
+	GENERATED_BODY()
+	
+};
