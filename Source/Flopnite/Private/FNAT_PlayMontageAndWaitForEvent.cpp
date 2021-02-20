452c7d9f6fa5578af9cb6b655f711604ed50618f
chris-gong <chris_gong@yahoo.com>
2021-02-20 11:37:13 -0500

    added PlayMontageAndWaitForEvent ability task from the ActionRPG and GASDocumentation projects

--- Source/Flopnite/Private/FNAT_PlayMontageAndWaitForEvent.cpp
+++ Source/Flopnite/Private/FNAT_PlayMontageAndWaitForEvent.cpp
@@ -3,19 +3,262 @@
 
 #include "FNAT_PlayMontageAndWaitForEvent.h"
 #include "AbilitySystemGlobals.h"
+#include "FNAbilitySystemComponent.h"
+#include "GameFramework/Character.h"
 
-UFNAT_PlayMontageAndWaitForEvent* UFNAT_PlayMontageAndWaitForEvent::CreatePlayMontageAndWaitForEventProxy(UGameplayAbility* OwningAbility, FName TaskInstanceName,
-	UAnimMontage* AnimMontageToPlay, FGameplayTagContainer EventTags, float PlayRate, FName AnimStartSection, bool bStopWhenGameplayAbilityEnds, float RootMotionTranslationScale, float AnimStartTimeSeconds)
+UFNAT_PlayMontageAndWaitForEvent::UFNAT_PlayMontageAndWaitForEvent(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
 {
-	UAbilitySystemGlobals::NonShipping_ApplyGlobalAbilityScaler_Rate(PlayRate);
+	Rate = 1.f;
+	bStopWhenAbilityEnds = true;
+}
 
-	UFNAT_PlayMontageAndWaitForEvent* UFNAT_PlayMontageAndWaitForEventObj = NewAbilityTask<UFNAT_PlayMontageAndWaitForEvent>(OwningAbility, TaskInstanceName);
-	UFNAT_PlayMontageAndWaitForEventObj->EventTags = EventTags;
+UFNAbilitySystemComponent* UFNAT_PlayMontageAndWaitForEvent::GetTargetASC()
+{
+	return Cast<UFNAbilitySystemComponent>(AbilitySystemComponent);
+}
 
-	return UFNAT_PlayMontageAndWaitForEventObj;
+void UFNAT_PlayMontageAndWaitForEvent::OnMontageBlendingOut(UAnimMontage* Montage, bool bInterrupted)
+{
+	if (Ability && Ability->GetCurrentMontage() == MontageToPlay)
+	{
+		if (Montage == MontageToPlay)
+		{
+			AbilitySystemComponent->ClearAnimatingAbility(Ability);
+
+			// Reset AnimRootMotionTranslationScale
+			ACharacter* Character = Cast<ACharacter>(GetAvatarActor());
+			if (Character && (Character->GetLocalRole() == ROLE_Authority ||
+				(Character->GetLocalRole() == ROLE_AutonomousProxy && Ability->GetNetExecutionPolicy() == EGameplayAbilityNetExecutionPolicy::LocalPredicted)))
+			{
+				Character->SetAnimRootMotionTranslationScale(1.f);
+			}
+
+		}
+	}
+
+	if (bInterrupted)
+	{
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			OnInterrupted.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+	else
+	{
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			OnBlendOut.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+}
+
+void UFNAT_PlayMontageAndWaitForEvent::OnAbilityCancelled()
+{
+	// TODO: Merge this fix back to engine, it was calling the wrong callback
+
+	if (StopPlayingMontage())
+	{
+		// Let the BP handle the interrupt as well
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			OnCancelled.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+}
+
+void UFNAT_PlayMontageAndWaitForEvent::OnMontageEnded(UAnimMontage* Montage, bool bInterrupted)
+{
+	if (!bInterrupted)
+	{
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			OnCompleted.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+
+	EndTask();
+}
+
+void UFNAT_PlayMontageAndWaitForEvent::OnGameplayEvent(FGameplayTag EventTag, const FGameplayEventData* Payload)
+{
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		FGameplayEventData TempData = *Payload;
+		TempData.EventTag = EventTag;
+
+		EventReceived.Broadcast(EventTag, TempData);
+	}
+}
+
+UFNAT_PlayMontageAndWaitForEvent* UFNAT_PlayMontageAndWaitForEvent::PlayMontageAndWaitForEvent(UGameplayAbility* OwningAbility,
+	FName TaskInstanceName, UAnimMontage* MontageToPlay, FGameplayTagContainer EventTags, float Rate, FName StartSection, bool bStopWhenAbilityEnds, float AnimRootMotionTranslationScale)
+{
+	UAbilitySystemGlobals::NonShipping_ApplyGlobalAbilityScaler_Rate(Rate);
+
+	UFNAT_PlayMontageAndWaitForEvent* AbilityTaskObj = NewAbilityTask<UFNAT_PlayMontageAndWaitForEvent>(OwningAbility, TaskInstanceName);
+	AbilityTaskObj->MontageToPlay = MontageToPlay;
+	AbilityTaskObj->EventTags = EventTags;
+	AbilityTaskObj->Rate = Rate;
+	AbilityTaskObj->StartSection = StartSection;
+	AbilityTaskObj->AnimRootMotionTranslationScale = AnimRootMotionTranslationScale;
+	AbilityTaskObj->bStopWhenAbilityEnds = bStopWhenAbilityEnds;
+
+	return AbilityTaskObj;
 }
 
 void UFNAT_PlayMontageAndWaitForEvent::Activate()
 {
-	UE_LOG(LogTemp, Warning, TEXT("AbilityTask actiavated"));
+	if (Ability == nullptr)
+	{
+		return;
+	}
+
+	bool bPlayedMontage = false;
+	UFNAbilitySystemComponent* FNAbilitySystemComponent = GetTargetASC();
+
+	if (FNAbilitySystemComponent)
+	{
+		const FGameplayAbilityActorInfo* ActorInfo = Ability->GetCurrentActorInfo();
+		UAnimInstance* AnimInstance = ActorInfo->GetAnimInstance();
+		if (AnimInstance != nullptr)
+		{
+			// Bind to event callback
+			EventHandle = FNAbilitySystemComponent->AddGameplayEventTagContainerDelegate(EventTags, FGameplayEventTagMulticastDelegate::FDelegate::CreateUObject(this, &UFNAT_PlayMontageAndWaitForEvent::OnGameplayEvent));
+
+			if (FNAbilitySystemComponent->PlayMontage(Ability, Ability->GetCurrentActivationInfo(), MontageToPlay, Rate, StartSection) > 0.f)
+			{
+				// Playing a montage could potentially fire off a callback into game code which could kill this ability! Early out if we are  pending kill.
+				if (ShouldBroadcastAbilityTaskDelegates() == false)
+				{
+					return;
+				}
+
+				CancelledHandle = Ability->OnGameplayAbilityCancelled.AddUObject(this, &UFNAT_PlayMontageAndWaitForEvent::OnAbilityCancelled);
+
+				BlendingOutDelegate.BindUObject(this, &UFNAT_PlayMontageAndWaitForEvent::OnMontageBlendingOut);
+				AnimInstance->Montage_SetBlendingOutDelegate(BlendingOutDelegate, MontageToPlay);
+
+				MontageEndedDelegate.BindUObject(this, &UFNAT_PlayMontageAndWaitForEvent::OnMontageEnded);
+				AnimInstance->Montage_SetEndDelegate(MontageEndedDelegate, MontageToPlay);
+
+				ACharacter* Character = Cast<ACharacter>(GetAvatarActor());
+				if (Character && (Character->GetLocalRole() == ROLE_Authority ||
+					(Character->GetLocalRole() == ROLE_AutonomousProxy && Ability->GetNetExecutionPolicy() == EGameplayAbilityNetExecutionPolicy::LocalPredicted)))
+				{
+					Character->SetAnimRootMotionTranslationScale(AnimRootMotionTranslationScale);
+				}
+
+				bPlayedMontage = true;
+			}
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("UFNAbilityTask_PlayMontageAndWaitForEvent call to PlayMontage failed!"));
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("UFNAbilityTask_PlayMontageAndWaitForEvent called on invalid AbilitySystemComponent"));
+	}
+
+	if (!bPlayedMontage)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("UFNAbilityTask_PlayMontageAndWaitForEvent called in Ability %s failed to play montage %s; Task Instance Name %s."), *Ability->GetName(), *GetNameSafe(MontageToPlay), *InstanceName.ToString());
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			//ABILITY_LOG(Display, TEXT("%s: OnCancelled"), *GetName());
+			OnCancelled.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+
+	SetWaitingOnAvatar();
+}
+
+void UFNAT_PlayMontageAndWaitForEvent::ExternalCancel()
+{
+	check(AbilitySystemComponent);
+
+	OnAbilityCancelled();
+
+	Super::ExternalCancel();
+}
+
+void UFNAT_PlayMontageAndWaitForEvent::OnDestroy(bool AbilityEnded)
+{
+	// Note: Clearing montage end delegate isn't necessary since its not a multicast and will be cleared when the next montage plays.
+	// (If we are destroyed, it will detect this and not do anything)
+
+	// This delegate, however, should be cleared as it is a multicast
+	if (Ability)
+	{
+		Ability->OnGameplayAbilityCancelled.Remove(CancelledHandle);
+		if (AbilityEnded && bStopWhenAbilityEnds)
+		{
+			StopPlayingMontage();
+		}
+	}
+
+	UFNAbilitySystemComponent* FNAbilitySystemComponent = GetTargetASC();
+	if (FNAbilitySystemComponent)
+	{
+		FNAbilitySystemComponent->RemoveGameplayEventTagContainerDelegate(EventTags, EventHandle);
+	}
+
+	Super::OnDestroy(AbilityEnded);
+
+}
+
+bool UFNAT_PlayMontageAndWaitForEvent::StopPlayingMontage()
+{
+	const FGameplayAbilityActorInfo* ActorInfo = Ability->GetCurrentActorInfo();
+	if (!ActorInfo)
+	{
+		return false;
+	}
+
+	UAnimInstance* AnimInstance = ActorInfo->GetAnimInstance();
+	if (AnimInstance == nullptr)
+	{
+		return false;
+	}
+
+	// Check if the montage is still playing
+	// The ability would have been interrupted, in which case we should automatically stop the montage
+	if (AbilitySystemComponent && Ability)
+	{
+		if (AbilitySystemComponent->GetAnimatingAbility() == Ability
+			&& AbilitySystemComponent->GetCurrentMontage() == MontageToPlay)
+		{
+			// Unbind delegates so they don't get called as well
+			FAnimMontageInstance* MontageInstance = AnimInstance->GetActiveInstanceForMontage(MontageToPlay);
+			if (MontageInstance)
+			{
+				MontageInstance->OnMontageBlendingOutStarted.Unbind();
+				MontageInstance->OnMontageEnded.Unbind();
+			}
+
+			AbilitySystemComponent->CurrentMontageStop();
+			return true;
+		}
+	}
+
+	return false;
+}
+
+FString UFNAT_PlayMontageAndWaitForEvent::GetDebugString() const
+{
+	UAnimMontage* PlayingMontage = nullptr;
+	if (Ability)
+	{
+		const FGameplayAbilityActorInfo* ActorInfo = Ability->GetCurrentActorInfo();
+		UAnimInstance* AnimInstance = ActorInfo->GetAnimInstance();
+
+		if (AnimInstance != nullptr)
+		{
+			PlayingMontage = AnimInstance->Montage_IsActive(MontageToPlay) ? MontageToPlay : AnimInstance->GetCurrentActiveMontage();
+		}
+	}
+
+	return FString::Printf(TEXT("PlayMontageAndWaitForEvent. MontageToPlay: %s  (Currently Playing): %s"), *GetNameSafe(MontageToPlay), *GetNameSafe(PlayingMontage));
 }
--- Source/Flopnite/Public/FNAT_PlayMontageAndWaitForEvent.h
+++ Source/Flopnite/Public/FNAT_PlayMontageAndWaitForEvent.h
@@ -3,11 +3,19 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "Abilities/Tasks/AbilityTask.h"
 #include "FNAT_PlayMontageAndWaitForEvent.generated.h"
 
+class UFNAbilitySystemComponent;
+
+/** Delegate type used, EventTag and Payload may be empty if it came from the montage callbacks */
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FFNPlayMontageAndWaitForEventDelegate, FGameplayTag, EventTag, FGameplayEventData, EventData);
+
 /**
- * 
+ * This task combines PlayMontageAndWait and WaitForEvent into one task, so you can wait for multiple types of activations such as from a melee combo
+ * Much of this code is copied from one of those two ability tasks
+ * This is a good task to look at as an example when creating game-specific tasks
+ * It is expected that each game will have a set of game-specific tasks to do what they want
  */
 UCLASS()
 class FLOPNITE_API UFNAT_PlayMontageAndWaitForEvent : public UAbilityTask
@@ -15,8 +23,53 @@ class FLOPNITE_API UFNAT_PlayMontageAndWaitForEvent : public UAbilityTask
 	GENERATED_BODY()
 	
 public:
+	// Constructor and overrides
+	UFNAT_PlayMontageAndWaitForEvent(const FObjectInitializer& ObjectInitializer);
+
+	/**
+	* The Blueprint node for this task, PlayMontageAndWaitForEvent, has some black magic from the plugin that automagically calls Activate()
+	* inside of K2Node_LatentAbilityCall as stated in the AbilityTask.h. Ability logic written in C++ probably needs to call Activate() itself manually.
+	*/
+	virtual void Activate() override;
+	virtual void ExternalCancel() override;
+	virtual FString GetDebugString() const override;
+	virtual void OnDestroy(bool AbilityEnded) override;
+
+	/** The montage completely finished playing */
+	UPROPERTY()
+	FFNPlayMontageAndWaitForEventDelegate OnCompleted;
+
+	/** The montage started blending out */
+	UPROPERTY()
+	FFNPlayMontageAndWaitForEventDelegate OnBlendOut;
+
+	/** The montage was interrupted */
+	UPROPERTY()
+	FFNPlayMontageAndWaitForEventDelegate OnInterrupted;
+
+	/** The ability task was explicitly cancelled by another ability */
+	UPROPERTY()
+	FFNPlayMontageAndWaitForEventDelegate OnCancelled;
+
+	/** One of the triggering gameplay events happened */
+	UPROPERTY()
+	FFNPlayMontageAndWaitForEventDelegate EventReceived;
+
+	/**
+	 * Play a montage and wait for it end. If a gameplay event happens that matches EventTags (or EventTags is empty), the EventReceived delegate will fire with a tag and event data.
+	 * If StopWhenAbilityEnds is true, this montage will be aborted if the ability ends normally. It is always stopped when the ability is explicitly cancelled.
+	 * On normal execution, OnBlendOut is called when the montage is blending out, and OnCompleted when it is completely done playing
+	 * OnInterrupted is called if another montage overwrites this, and OnCancelled is called if the ability or task is cancelled
+	 *
+	 * @param TaskInstanceName Set to override the name of this task, for later querying
+	 * @param MontageToPlay The montage to play on the character
+	 * @param EventTags Any gameplay events matching this tag will activate the EventReceived callback. If empty, all events will trigger callback
+	 * @param Rate Change to play the montage faster or slower
+	 * @param bStopWhenAbilityEnds If true, this montage will be aborted if the ability ends normally. It is always stopped when the ability is explicitly cancelled
+	 * @param AnimRootMotionTranslationScale Change to modify size of root motion or set to 0 to block it entirely
+	 */
 	UFUNCTION()
-	static UFNAT_PlayMontageAndWaitForEvent* CreatePlayMontageAndWaitForEventProxy(
+	static UFNAT_PlayMontageAndWaitForEvent* PlayMontageAndWaitForEvent(
 		UGameplayAbility* OwningAbility,
 		FName TaskInstanceName,
 		UAnimMontage* MontageToPlay,
@@ -24,13 +77,46 @@ public:
 		float Rate = 1.f,
 		FName StartSection = NAME_None,
 		bool bStopWhenAbilityEnds = true,
-		float AnimRootMotionTranslationScale = 1.f, 
-		float StartTimeSeconds = 0.f);
-
-	virtual void Activate() override;
+		float AnimRootMotionTranslationScale = 1.f);
 
 private:
+	/** Montage that is playing */
+	UPROPERTY()
+	UAnimMontage* MontageToPlay;
+
 	/** List of tags to match against gameplay events */
 	UPROPERTY()
 	FGameplayTagContainer EventTags;
+
+	/** Playback rate */
+	UPROPERTY()
+	float Rate;
+
+	/** Section to start montage from */
+	UPROPERTY()
+	FName StartSection;
+
+	/** Modifies how root motion movement to apply */
+	UPROPERTY()
+	float AnimRootMotionTranslationScale;
+
+	/** Rather montage should be aborted if ability ends */
+	UPROPERTY()
+	bool bStopWhenAbilityEnds;
+
+	/** Checks if the ability is playing a montage and stops that montage, returns true if a montage was stopped, false if not. */
+	bool StopPlayingMontage();
+
+	/** Returns our ability system component */
+	UFNAbilitySystemComponent* GetTargetASC();
+
+	void OnMontageBlendingOut(UAnimMontage* Montage, bool bInterrupted);
+	void OnAbilityCancelled();
+	void OnMontageEnded(UAnimMontage* Montage, bool bInterrupted);
+	void OnGameplayEvent(FGameplayTag EventTag, const FGameplayEventData* Payload);
+
+	FOnMontageBlendingOutStarted BlendingOutDelegate;
+	FOnMontageEnded MontageEndedDelegate;
+	FDelegateHandle CancelledHandle;
+	FDelegateHandle EventHandle;
 };
